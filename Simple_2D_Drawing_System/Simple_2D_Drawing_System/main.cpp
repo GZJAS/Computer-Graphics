f1acfaae3ff6f218e53aec93d83eddd8d21e8d38
Dimitar <dimdimvas@yahoo.com>
2017-01-14 00:27:00 -0800

    setPixel() created to convert the x and y coordinates that will be read in from a file to the appropriate index location inside the pixelBuffer.

--- Simple_2D_Drawing_System/Simple_2D_Drawing_System/main.cpp
+++ Simple_2D_Drawing_System/Simple_2D_Drawing_System/main.cpp
@@ -6,22 +6,63 @@
 //
 //
 
-
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#include <iostream>
 
 float *PixelBuffer;
+int window_width = 200;
+int window_height = 200;
+int PixelBufferSize = window_width * window_height * 3;
+
+
+void clearAllPixels();
+void setPixel(int x, int y, double c);
 void display();
-int main(int argc, char *argv[])
-{
+
+// clears all of the pixels from the screen
+void clearAllPixels(){
+    for(int i = 0; i < 200; i++){
+        for(int j = 0; j < 200; j++){
+            setPixel(i, j, 1.0);
+        }
+    }
+}
+
+// sets the local in pixelBuffer to c given x and y coordinates
+void setPixel(int x, int y, double c){
+    int i = (y * window_width + x) * 3;
+    for(int j = 0; j < 3; j++){
+        PixelBuffer[i + j] = c;
+    }
+    return;
+}
+
+// main display loop, this function will be called again and again by OpenGL
+void display(){
+    
+    //Misc.
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    //draws pixel on screen, width and height must match pixel buffer dimension
+    glDrawPixels(window_width, window_height, GL_RGB, GL_FLOAT, PixelBuffer);
+    
+    //window refresh
+    glFlush();
+}
+
+
+
+int main(int argc, char *argv[]){
     //allocate new pixel buffer, need initialization!!
-    PixelBuffer = new float[200 * 200 * 3];
+    PixelBuffer = new float[PixelBufferSize];
     
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
     //set window size to 200*200
-    glutInitWindowSize(200, 200);
+    glutInitWindowSize(window_width, window_height);
     //set window position
     glutInitWindowPosition(100, 100);
     
@@ -35,16 +76,4 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-//main display loop, this function will be called again and again by OpenGL
-void display()
-{
-    //Misc.
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-    
-    //draws pixel on screen, width and height must match pixel buffer dimension
-    glDrawPixels(200, 200, GL_RGB, GL_FLOAT, PixelBuffer);
-    
-    //window refresh
-    glFlush();
-}
+
