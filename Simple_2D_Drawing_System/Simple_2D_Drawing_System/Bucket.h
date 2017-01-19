22cd65065e65ea86aaa73d83e8299256667ccf6c
Dimitar <dimdimvas@yahoo.com>
2017-01-18 16:12:39 -0800

    Struct for holding Bucket information

--- Simple_2D_Drawing_System/Simple_2D_Drawing_System/Bucket.h
+++ Simple_2D_Drawing_System/Simple_2D_Drawing_System/Bucket.h
@@ -1,13 +1,24 @@
 //
-//  Bucket.h
+//  Vertex.h
 //  Simple_2D_Drawing_System
 //
 //  Created by Dimitar Vasilev on 1/18/17.
 //
 //
 
-#ifndef Bucket_h
-#define Bucket_h
+#ifndef Vertex_h
+#define Vertex_h
+/*
+ Bucket struct to hold edge information
+ */
+struct Bucket {
+    int yMax;       // Maximum Y position of the edge
+    int yMin;       // Minimum Y position of the edge
+    int x;          // The current x position along the scan line, initially start at the same point as the yMin of the edge
+    int sign;       // The sign of the edge's slope
+    int dX;         // The absolute delta x b/w edge's vertex points
+    int dY;         // The absolute delta y b/w edge's vertex points
+    double sum;     // Initiated to 0. Used as the scan lines are being filled to x to the next position
+};
 
-
-#endif /* Bucket_h */
+#endif /* Vertex_h */
