24d736e2097b320ca62c56f7f3ff625fe70a4a98
Dimitar <dimdimvas@yahoo.com>
2017-01-18 16:47:48 -0800

    Project updated. Rasterization of polygons.

---
+++ Simple_2D_Drawing_System/build/Simple_2D_Drawing_System.build/Debug/Simple_2D_Drawing_System.build/StaticAnalyzer/Simple_2D_Drawing_System/Simple_2D_Drawing_System/normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Dimitar/Documents/Computer\ Graphics/Simple_2D_Drawing_System/Simple_2D_Drawing_System/main.cpp \
+  /Users/Dimitar/Documents/Computer\ Graphics/Simple_2D_Drawing_System/Simple_2D_Drawing_System/Bucket.h
