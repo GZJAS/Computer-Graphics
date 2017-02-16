92fd0e221f2e89a7cf94eda55eb597163f4dab53
Dimitar <dimdimvas@yahoo.com>
2017-02-15 20:12:26 -0800

    Remove build files from git tracking

--- Project 2/build/Simple_2D_Drawing_System.build/Debug/Simple_2D_Drawing_System.build/Objects-normal/x86_64/Line.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Line.cpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Line.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Geometry.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Point.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Edge.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Bucket.hpp
--- Project 2/build/Simple_2D_Drawing_System.build/Debug/Simple_2D_Drawing_System.build/Objects-normal/x86_64/main.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/main.cpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Line.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Geometry.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Point.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Edge.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Bucket.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/Polygon.hpp \
-  /Users/Dimitar/Documents/Computer\ Graphics/Project\ 2/Simple_2D_Drawing_System/threeD.hpp
